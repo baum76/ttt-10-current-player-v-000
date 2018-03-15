20f7179d0df58fb5fce220947c2b51bca28d183f
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-15 19:27:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,7 @@ def turn_count(board)
   counter = 0
   board.each do |space|
     if space == "X" || space == "O"
-      counter += 1
-    else
-      puts "3"  
+      counter += 1 
     end
   end
 end
